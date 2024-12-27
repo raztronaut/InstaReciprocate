355ee030bf56742fbbb4a087f6d474c9af0b8854
Razi Syed <syed.raziulhaque@gmail.com>
2024-12-26 21:31:23 -0500

    fix: update config files to use correct relative paths

--- config/build-extension.sh
+++ config/build-extension.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Change to root directory
+cd "$(dirname "$0")/.."
+
 # Clean dist directory
 rm -rf dist-extension
 mkdir -p dist-extension/assets
 
 # Build the extension
-npx webpack --config config/webpack.config.js
+npx webpack --config ./config/webpack.config.js
 
 # Copy static assets
 cp -r src/assets/icons dist-extension/assets/
